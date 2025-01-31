6c0daf0b4c89181c625ac5888c8cabe5e10cb264
Lyonyai <135249178+Lyonyai@users.noreply.github.com>
2025-01-31 11:01:31 +0300

    Звуки + убраны юзинги

--- Assets/Scripts/DamageDealer.cs
+++ Assets/Scripts/DamageDealer.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
-using static Unity.VisualScripting.Member;
 
 public class DamageDealer : MonoBehaviour
 {
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -1,6 +1,5 @@
 
 using UnityEngine;
-using static Unity.VisualScripting.Member;
 
 public class Projectile : MonoBehaviour
 {
